1e140a56b135191e974149057253c9528e04dc39
Pearl276 <palak.pearl94@gmail.com>
2025-02-09 18:32:53 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,7 +3,7 @@ int main() {
     int a,b;
     char op;
     scanf("%d %d", &a,&b);
-    scanf("%c", &op);
+    scanf(" %c", &op);
     if (op=='+'){
         printf("%d", a+b);
     }
@@ -14,7 +14,12 @@ int main() {
         printf("%d", a*b);
     }
     else if(op== '/'){
-        printf("%d", a/b);
+        if (b!=0){
+            printf("%d", a/b);
+        }
+        else{
+            printf("error");
+        }
     }
     else{
         printf("error");
