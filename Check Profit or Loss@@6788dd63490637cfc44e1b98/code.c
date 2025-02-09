023163fc86927007f76fc794d4ddba5d97c6d43d
Pearl276 <palak.pearl94@gmail.com>
2025-02-09 16:08:22 +0530

    Solution to Check Profit or Loss

---
+++ Check Profit or Loss@@6788dd63490637cfc44e1b98/code.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int SP, CP;
+    scanf("%d %d", &SP, &CP);
+    if(SP>CP){
+        printf("Profit");
+    }
+    else if(CP>SP){
+        printf("Loss");
+    }
+    else{
+        printf("No Profit No Loss");
+    }
+    return 0;
+}
